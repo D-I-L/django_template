c3a7f9ed102f6da078e07a9811d3c53678c95e61
tcarver <tjc29@duffield>
2015-04-07 16:28:45 +0100

    urlpatterns is a list of django.conf.urls.url() instances

--- django_template/local_apps/gene/urls.py
+++ django_template/local_apps/gene/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from gene import views
 
-urlpatterns = patterns('genes',
-                       url(r'^(?P<gene>[-\w]+)/$',
-                           views.gene_page, name='gene_page'),
-                       )
+urlpatterns = [url(r'^(?P<gene>[-\w]+)/$', views.gene_page, name='gene_page'),
+               ]
