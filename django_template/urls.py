bf594156b0428d74dd4755419f105c8e61207bb1
tcarver <tjc29@duffield>
2015-05-14 11:40:14 +0100

    add elastic tastypie api registration

--- django_template/urls.py
+++ django_template/urls.py
@@ -5,6 +5,8 @@ from db.api import FeaturelocFullResource, FeatureResource, FeatureFullResource
 from db.api import FeaturepropResource, FeaturepropFullResource
 from db.api import OrganismResource
 from tastypie.api import Api
+from elastic.tastypie.api import GeneResource, GwasBarrettResource,\
+    MarkerResource
 
 # register tastypie api
 api = Api(api_name='dev')
@@ -20,8 +22,15 @@ api.register(FeaturepropResource())
 api.register(FeaturepropFullResource())
 api.register(CvtermpropResource())
 
+# register tastypie api
+elastic_api = Api(api_name='dev')
+elastic_api.register(GeneResource())
+elastic_api.register(GwasBarrettResource())
+elastic_api.register(MarkerResource())
+
 urlpatterns = [url(r'^', include('bands.urls', namespace="bands")),
                url(r'^api/', include(api.urls)),
+               url(r'^elastic/', include(elastic_api.urls)),
                url(r'^search/', include('elastic.urls', namespace="elastic")),
                url(r'^gene/', include('gene.urls')),
                url(r'^marker/', include('marker.urls')),
